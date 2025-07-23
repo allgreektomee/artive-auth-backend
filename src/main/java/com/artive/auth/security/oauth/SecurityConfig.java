7418fcbdf0fc48a9acb5b29d4a0f460942a3ff36
devcation <88315076+devcation-learn@users.noreply.github.com>
2025-07-23 10:28:53 +0900

    WebConfig
    WebConfig

--- src/main/java/com/artive/auth/security/oauth/SecurityConfig.java
+++ src/main/java/com/artive/auth/security/oauth/SecurityConfig.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -29,6 +30,7 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http, JwtAuthenticationFilter jwtFilter) throws Exception {
 
         return http
+                .cors(Customizer.withDefaults())
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/auth/**", "/mail/**", "/oauth2/**", "/swagger-ui/**", "/v3/api-docs/**","/api/artwork/**").permitAll()
